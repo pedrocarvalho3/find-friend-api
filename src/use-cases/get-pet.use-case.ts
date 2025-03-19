96b238b7949de8c1608b1e76bdc95080bb90d6f4
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-03-19 20:24:36 -0300

    feat: get pet use case

---
+++ src/use-cases/get-pet.use-case.ts
@@ -0,0 +1,25 @@
+import type { PetsRepository } from '@/repositories/pets.repository'
+import type { Pet } from '@prisma/client'
+import { PetNotFoundError } from './errors/pet-not-found.error'
+
+interface GetPetUseCaseRequest {
+  id: string
+}
+
+interface GetPetUseCaseResponse {
+  pet: Pet
+}
+
+export class GetPetUseCase {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute({ id }: GetPetUseCaseRequest): Promise<GetPetUseCaseResponse> {
+    const pet = await this.petsRepository.findById(id)
+
+    if (!pet) {
+      throw new PetNotFoundError()
+    }
+
+    return { pet }
+  }
+}
