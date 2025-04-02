174e63f419183aa77cd5f6967ec3b151fc53edef
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-04-02 20:47:14 -0300

    test: :white_check_mark: adding profile e2e test

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,37 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  // isAdmin = false,
+) {
+  await prisma.org.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      author_name: 'John Doe',
+      whatsapp: '11999999999',
+      cep: '12345678',
+      state: 'SP',
+      city: 'São Paulo',
+      neighborhood: 'Bairro',
+      street: 'Rua',
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    },
+  })
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    email: 'johndoe@example.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return {
+    token,
+  }
+}
