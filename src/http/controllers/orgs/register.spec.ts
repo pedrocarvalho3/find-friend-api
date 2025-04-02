296bca6056bb42ef583a6d7d06b05166b52f42e3
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-04-02 20:45:48 -0300

    test: :test_tube: adding register e2e test

---
+++ src/http/controllers/orgs/register.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register org', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      author_name: 'John Doe',
+      whatsapp: '11999999999',
+      cep: '12345678',
+      state: 'SP',
+      city: 'São Paulo',
+      neighborhood: 'Bairro',
+      street: 'Rua',
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    expect(response.status).toEqual(201)
+  })
+})
